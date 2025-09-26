303f7c390ab8a231cd43b08cd22e2b9a3eef23cb
Theo-D <theodessaigne@hotmail.fr>
2025-09-26 10:59:37 +0200

    used const to declare size and used === to check condition instead of ==

--- challenge/1-print_square.js
+++ challenge/1-print_square.js
@@ -7,14 +7,14 @@
 */
 
 
-if (process.argv[2] == null || isNaN(process.argv[2])) {
+if (process.argv[2] === undefined || isNaN(process.argv[2])) {
     process.stderr.write("Missing argument\n");
     process.stderr.write("Usage: ./1-print_square.js <size>\n");
     process.stderr.write("Example: ./1-print_square.js 8\n");
     process.exit(1)
 }
 
-size = parseInt(process.argv[2])
+const size = parseInt(process.argv[2], 10)
 
 for (let i = 0 ; i < size ; i ++) {
     for (let j = 0 ; j < size ; j ++) {
